8e5449b7c3e7e0beb2ee0899f4c2c13a6457c08e
Tim Wanders <timwanders241@gmail.com>
2017-05-26 09:02:22 +0200

    Added OUT, SendMessage and IRCconnect

--- TimsIRCBot/TimsIRCBot/TimsIRCBot.cs
+++ TimsIRCBot/TimsIRCBot/TimsIRCBot.cs
@@ -7,28 +7,47 @@ namespace TimsIRCBot
 {
 	class Program
 	{
+		internal static string input;
+		internal static string IRCnick = "TimsIRCBot";
+		internal static string[] IRCchannels = File.ReadAllLines("channels.txt");
+		internal static string IRCservaddr = "chat.freenode.net";
+		internal static string IRCuser = "USER " + IRCnick + " 0 * :" + IRCnick;
+		internal static int IRCservport = 6667;
+		internal static TcpClient IRCserv;
+		internal static NetworkStream IRCstream;
+		internal static StreamReader IRCreader;
+		internal static StreamWriter IRCwriter;
+		internal static void IRCconnect()
+		{
+			IRCserv = new TcpClient(IRCservaddr, IRCservport);
+			IRCstream = IRCserv.GetStream();
+			IRCreader = new StreamReader(IRCstream);
+			IRCwriter = new StreamWriter(IRCstream);
+			IRCwriter.WriteLine("NICK " + IRCnick);
+			IRCwriter.Flush();
+			IRCwriter.WriteLine(IRCuser);
+			IRCwriter.Flush();
+		}
+		internal static void OUT(string output) {
+			Console.WriteLine(">> " + output);
+		}
+		internal static void SendMessage(string message, string user)
+		{
+			message = ":" + message;
+			string msg = "PRIVMSG " + user + " " + message;
+			IRCwriter.WriteLine(msg);
+			IRCwriter.Flush();
+			OUT(msg);
+		}
 		static void Main(string[] args) 
 		{
 			if (!File.Exists("channels.txt")){
 				Console.WriteLine("Error: channels.txt required");
 				Environment.Exit(1);
 			}
-			string input;
-			string IRCnick = "TimsIRCBot";
-			string IRCservaddr = "chat.freenode.net";
-			string[] IRCchannels = File.ReadAllLines("channels.txt");
-			int IRCservport = 6667;
-			string IRCuser = "USER " + IRCnick + " 0 * :" + IRCnick; 
-			TcpClient IRCserv = new TcpClient(IRCservaddr, IRCservport);
-			NetworkStream IRCstream = IRCserv.GetStream();
-			StreamReader IRCreader = new StreamReader(IRCstream);
-			StreamWriter IRCwriter = new StreamWriter(IRCstream);
-			IRCwriter.WriteLine("NICK " + IRCnick);
-			IRCwriter.Flush();
-			IRCwriter.WriteLine(IRCuser);
-			IRCwriter.Flush();
+			IRCconnect();
 			while (true)
-				{
+			{
 				while ((input = IRCreader.ReadLine()) != null)
 				{
 					Console.WriteLine("<< " + input);
@@ -38,11 +57,25 @@ namespace TimsIRCBot
 						IRCwriter.WriteLine("PONG " + splitinput[1]);
 						IRCwriter.Flush();
 					}
-					if (splitinput[1] == "001"){
+					if (splitinput[1] == "001")
+					{
 						foreach (string IRCchannel in IRCchannels)
 						{
 							IRCwriter.WriteLine("JOIN " + IRCchannel);
 							IRCwriter.Flush();
+						}
+					}
+					if (splitinput.LongLength >= 4)
+					{
+						string IRCmsgreciever = splitinput[2];
+						string IRCmessage = splitinput[3].Remove(0, 1);
+						switch (IRCmessage)
+						{
+							case "Hello":
+								SendMessage("Hi", IRCmsgreciever);
+								break;
+							default:
+								break;
 						}
 					}
 				}
